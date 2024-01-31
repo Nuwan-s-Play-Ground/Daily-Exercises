72f75ed9295b1e5f1ed197cd54c2cf555011e205
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-31 17:38:17 +0530

    chore: Add Java code to print system properties
    - Implement a simple Java program in the Test class
    - Iterate through System properties and print each key
    
    This commit provides a basic demonstration of accessing and printing system properties using a for loop.

--- spring/UserController.java
+++ spring/UserController.java
@@ -1,23 +1,23 @@
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/users")
-public class UserController {
+// @Controller
+// @RequestMapping("/users")
+// public class UserController {
 
-    @PostMapping("/add")
-    public String addUser(@ModelAttribute("user") User user) {
-        // Process the user object obtained from the request body
-        System.out.println("Received user: " + user.getUsername() + ", " + user.getPassword());
+//     @PostMapping("/add")
+//     public String addUser(@ModelAttribute("user") User user) {
+//         // Process the user object obtained from the request body
+//         System.out.println("Received user: " + user.getUsername() + ", " + user.getPassword());
 
-        // Perform some logic with the user data (e.g., save to a database)
+//         // Perform some logic with the user data (e.g., save to a database)
 
-        // Redirect to a success page or return a response
-        return "redirect:/success";
-    }
+//         // Redirect to a success page or return a response
+//         return "redirect:/success";
+//     }
 
-    // Additional methods and mappings can be added here
-}
+//     // Additional methods and mappings can be added here
+// }
